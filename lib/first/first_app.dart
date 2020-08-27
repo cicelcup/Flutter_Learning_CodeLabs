851da3b686efbf0959170f55ee26ddaac60a6a06
JAPM <cicelcup@gmail.com>
2020-08-27 08:57:18 -0600

    Add first CodeLab screen

--- lib/first/first_app.dart
+++ lib/first/first_app.dart
@@ -1,9 +1,19 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttercodelabs/general/app_strings_i18n.dart';
 
 //First CodeLab
 class FirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("JAPM");
+    final wordPair = WordPair.random();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(AppStringsI18N.codeLabTitle1),
+      ),
+      body: Center(
+        child: Text("${wordPair.first} - ${wordPair.second}"),
+      ),
+    );
   }
 }
