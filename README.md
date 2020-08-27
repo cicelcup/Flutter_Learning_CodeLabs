acbdedca6be88172deaf60cb9307ea4741b9bfb6
JAPM <cicelcup@gmail.com>
2020-08-27 07:27:05 -0600

    Start project

---
+++ README.md
@@ -0,0 +1,3 @@
+# fluttercodelabs
+
+A new Flutter application with all code labs available
---
+++ android/app/src/main/kotlin/com/cicelcup/fluttercodelabs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cicelcup.fluttercodelabs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
