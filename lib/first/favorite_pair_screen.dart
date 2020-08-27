bf4847a1a9a08333304ab48f05ba1f4bdb799222
JAPM <cicelcup@gmail.com>
2020-08-27 11:43:08 -0600

    Add items in favorite list

--- lib/first/favorite_pair_screen.dart
+++ lib/first/favorite_pair_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttercodelabs/first/provider/first_app_provider.dart';
+import 'package:fluttercodelabs/general/app_strings_i18n.dart';
 import 'package:provider/provider.dart';
 
 class FavoritePairScreen extends StatelessWidget {
@@ -8,6 +9,18 @@ class FavoritePairScreen extends StatelessWidget {
     final _favoriteList = context
         .select<FirstAppProvider, Set>((provider) => provider.savedWords);
 
-    return Text("${_favoriteList.length}");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppStringsI18N.favoritePairScreenAppBarTitle),
+      ),
+      body: ListView(
+        children: ListTile.divideTiles(
+          context: context,
+          tiles: _favoriteList.map(
+            (e) => ListTile(title: Text(e.toString())),
+          ),
+        ).toList(),
+      ),
+    );
   }
 }
--- lib/general/app_strings_i18n.dart
+++ lib/general/app_strings_i18n.dart
@@ -3,4 +3,5 @@ class AppStringsI18N {
   static const String appTitle = "Flutter CodeLabs";
   static const String mainScreenAppBarTitle = "CodeLabs";
   static const String codeLabTitle1 = "Your first flutter app";
+  static const String favoritePairScreenAppBarTitle = "Favorite pairs";
 }
